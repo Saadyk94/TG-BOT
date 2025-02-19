f1c48ece6c9f9ab3b67a3b44138cff87ff45187e
Saadyk <117447098+Saadyk94@users.noreply.github.com>
2025-02-19 17:03:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-open_weather_token = "cfcda8f2d7eefec11ed178e8074c0c20"
-tg_bot_token = "5724187961:AAGPIsLRErwrO9cGJalwcGxWRF5Y3_mIhAk"
-tg_chatgpt_token = "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"
+open_weather_token = ""
+tg_bot_token = ""
+tg_chatgpt_token = ""
